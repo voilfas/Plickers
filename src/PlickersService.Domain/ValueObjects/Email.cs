b38b95fff7e34adae117675fbe101ae7f8159bd0
Artyom <sobkalovt@bk.ru>
2026-08-11 19:37:09 +0300

    feat: add value objects: Email, Answer, FullName

---
+++ src/PlickersService.Domain/ValueObjects/Email.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Mail;
+using PlickersService.Domain.Errors;
+using PlickersService.Domain.Results;
+
+namespace PlickersService.Domain.ValueObjects;
+
+public record Email
+{
+    public string Value { get; init; }
+
+    private Email() { } // Для EF Core / сериализации
+
+    private Email(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<Email> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return Result<Email>.Failure(DomainErrors.User.EmptyEmail);
+
+        string trimmedEmail = value.Trim();
+
+        if (trimmedEmail.Length > 100)
+            return Result<Email>.Failure(DomainErrors.User.InvalidEmailLength);
+        
+        try
+        {
+            var mailAddress = new MailAddress(trimmedEmail);
+            
+            if (!mailAddress.Address.Contains('.'))
+                return Result<Email>.Failure(DomainErrors.User.InvalidEmailFormat);
+        }
+        catch (FormatException)
+        {
+            return Result<Email>.Failure(DomainErrors.User.InvalidEmailFormat);
+        }
+
+        return Result<Email>.Success(new Email(trimmedEmail));
+    }
+}
---
+++ src/PlickersService.Domain/ValueObjects/FullName.cs
@@ -0,0 +1,56 @@
+﻿using PlickersService.Domain.Errors;
+using PlickersService.Domain.Results;
+
+namespace PlickersService.Domain.ValueObjects;
+
+public record FullName
+{
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public string MiddleName { get; init; }
+    
+
+    #pragma  warning disable CS8618 // Non-nullable field is uninitialized.
+    private FullName() { }
+    #pragma warning disable CS8618 // Non-nullable field is uninitialized.
+
+    private FullName(
+        string firstName,
+        string lastName,
+        string middleName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        MiddleName = middleName;
+    }
+
+    public static Result<FullName> Create(
+        string firstName,
+        string lastName,
+        string middleName)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            return Result<FullName>.Failure(DomainErrors.User.EmptyFirstName);
+        
+        var trimmedFirstName = firstName.Trim();
+        if (trimmedFirstName.Length is < 2 or > 30)
+            return Result<FullName>.Failure(DomainErrors.User.InvalidFirstNameLength);
+        
+        if (string.IsNullOrWhiteSpace(lastName))
+            return Result<FullName>.Failure(DomainErrors.User.EmptyLastName);
+        
+        var trimmedLastName = lastName.Trim();
+        if (trimmedLastName.Length is < 2 or > 30)
+            return Result<FullName>.Failure(DomainErrors.User.InvalidLastNameLength);
+        
+        if (string.IsNullOrWhiteSpace(middleName))
+                    return Result<FullName>.Failure(DomainErrors.User.EmptyMiddleName);
+        
+        var trimmedMiddleName = lastName.Trim();
+        if (trimmedMiddleName.Length is < 2 or > 30)
+            return Result<FullName>.Failure(DomainErrors.User.InvalidMiddleNameLength);
+        
+
+        return Result<FullName>.Success(new FullName(trimmedFirstName, trimmedLastName, trimmedMiddleName));
+    }
+}
