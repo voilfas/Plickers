017e7be18a8750bfc34794b7769ad76859493550
Artyom <sobkalovt@bk.ru>
2026-08-11 19:35:18 +0300

    feat: add BaseEntity

---
+++ src/PlickersService.Domain/Abstract/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace PlickersService.Domain.Abstract;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; private set; }
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+    }
+    
+    protected BaseEntity(Guid id)
+    {
+        Id = id;
+    }
+}
