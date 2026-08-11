57d72e8588d6b5ddd4111c054e2e4d48a86dac7b
Artyom <sobkalovt@bk.ru>
2026-08-11 19:34:37 +0300

    feat: add Result pattern

---
+++ src/PlickersService.Domain/Results/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace PlickersService.Domain.Results;
+
+public class Error
+{
+    public string Code { get; private set; }
+    public string Message { get; private set; }
+
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+}
---
+++ src/PlickersService.Domain/Results/ResultT.cs
@@ -0,0 +1,28 @@
+﻿namespace PlickersService.Domain.Results;
+
+public class Result<T> : Result
+{
+    public T? Value { get; private set; }
+    
+    private Result(T value) 
+        : base(isSuccess: true, error: null)
+    {
+        Value = value;
+    }
+
+    private Result(Error error) 
+        : base(isSuccess: false ,error: error)
+    {
+        Value = default;
+    }
+
+    public static Result<T> Success(T value)
+    {
+        return new Result<T>(value);
+    }
+
+    public new static Result<T> Failure(Error error)
+    {
+        return new Result<T>(error);
+    }
+}
