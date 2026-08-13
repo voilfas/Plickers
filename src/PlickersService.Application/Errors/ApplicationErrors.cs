e90a4dc33d0033ee426136f71159af2f7903ab66
Artyom <sobkalovt@bk.ru>
2026-08-13 13:47:06 +0300

    feat: add ApplicationErrors

---
+++ src/PlickersService.Application/Errors/ApplicationErrors.cs
@@ -0,0 +1,18 @@
+﻿using PlickersService.Domain.Results;
+
+namespace PlickersService.Application.Errors;
+
+public static class ApplicationErrors
+{
+    public static class Card
+    {
+        public static readonly Error NotFound = 
+            new Error("Card.NotFound", "Card not found in db");
+    }
+
+    public static class Pack
+    {
+        public static readonly Error NotFound = 
+            new Error("Pack.NotFound", "Pack not found in db");
+    }
+}
