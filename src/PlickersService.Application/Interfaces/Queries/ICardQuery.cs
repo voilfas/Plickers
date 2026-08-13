901157d2f6b650bc740d44c75e50c411e6c2bba5
Artyom <sobkalovt@bk.ru>
2026-08-13 13:42:56 +0300

    feat: add queries interfaces

---
+++ src/PlickersService.Application/Interfaces/Queries/ICardQuery.cs
@@ -0,0 +1,8 @@
+﻿using PlickersService.Application.DTOsResponse;
+
+namespace PlickersService.Application.Interfaces.Queries;
+
+public interface ICardQuery
+{
+    Task<CardResponse?> GetByIdAsync(Guid cardId, CancellationToken ct);
+}
