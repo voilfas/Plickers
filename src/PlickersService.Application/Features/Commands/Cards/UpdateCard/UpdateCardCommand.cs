5d7deebcd92290d60af95c1be2614217dff7b87a
Artyom <sobkalovt@bk.ru>
2026-08-13 13:45:10 +0300

    feat: add commands use cases for Card

---
+++ src/PlickersService.Application/Features/Commands/Cards/UpdateCard/UpdateCardCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using PlickersService.Application.DTOsResponse;
+using PlickersService.Domain.Results;
+
+namespace PlickersService.Application.Features.Cards.UpdateCard;
+
+public record UpdateCardCommand(
+    Guid CardId,
+    string NewName,
+    string NewQuestion,
+    string? NewPicturePath,
+    IReadOnlyCollection<CreateAnswerResponse> NewAnswers
+    ) : IRequest<Result>;
