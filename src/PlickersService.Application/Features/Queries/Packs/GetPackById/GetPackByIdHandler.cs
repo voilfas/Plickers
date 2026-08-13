87787678dc19f1ab0b79bb7bfd5c3d5334e7eebd
Artyom <sobkalovt@bk.ru>
2026-08-13 13:46:27 +0300

    feat: add queries use cases for Pack

---
+++ src/PlickersService.Application/Features/Queries/Packs/GetPackById/GetPackByIdHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using PlickersService.Application.DTOsResponse;
+using PlickersService.Application.Errors;
+using PlickersService.Application.Interfaces.Queries;
+using PlickersService.Domain.Results;
+
+namespace PlickersService.Application.Features.Queries.Packs.GetPackById;
+
+public class GetPackByIdHandler 
+    : IRequestHandler<GetPackByIdQuery, Result<PackResponse>>
+{
+    private readonly IPackQuery _packQuery;
+
+    public GetPackByIdHandler(IPackQuery packQuery)
+    {
+        _packQuery = packQuery;
+    }
+    
+    public async Task<Result<PackResponse>> Handle(
+        GetPackByIdQuery request,
+        CancellationToken ct)
+    {
+        var packResponseResult = await _packQuery.GetByIdAsync(request.PackId, ct);
+        
+        if (packResponseResult is null)
+            return Result<PackResponse>.Failure(ApplicationErrors.Pack.NotFound);
+        
+        return Result<PackResponse>.Success(packResponseResult);
+    }
+}
