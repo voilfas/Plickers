1045c69e3bb933ff5cf5e02aea6ff4ec8f4749c3
Artyom <sobkalovt@bk.ru>
2026-08-13 13:48:29 +0300

    feat: add DI

---
+++ src/PlickersService.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PlickersService.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
+        
+        return services;
+    }
+}
